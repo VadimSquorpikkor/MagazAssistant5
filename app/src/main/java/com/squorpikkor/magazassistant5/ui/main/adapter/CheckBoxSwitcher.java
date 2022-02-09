f4761304ac70eeba91c68049b09449f811ab42f4
VadimSquorpikkor <uniqueehub1024>
2022-02-09 13:45:03 +0300

    1.08 - 09/02/21 1. Работаю над чекбоксами. CheckBoxSwitcher

---
+++ app/src/main/java/com/squorpikkor/magazassistant5/ui/main/adapter/CheckBoxSwitcher.java
@@ -0,0 +1,33 @@
+package com.squorpikkor.magazassistant5.ui.main.adapter;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+
+import com.squorpikkor.magazassistant5.ui.main.entities.Employee;
+
+class CheckBoxSwitcher {
+
+    static void setCheckboxesByEmployee(CheckBox[] checks, Employee employee, int workingDaysCount) {
+        for (int i = 0; i < checks.length; i++) {
+            //перебираем все 9 чекбоксов
+            checks[i].setVisibility(workingDaysCount>i? View.VISIBLE:View.GONE);//оставляем количество чекбоксов равное количеству рабочих дней
+            if (employee.getDaysString().length()<i) checks[i].setChecked(true);//если чекбоксов больше, чем сохраненных то все чекбоксы сверх сохраненных чекаем
+
+            Log.e("TAG", "*** employee.getDaysString().length(): "+employee.getDaysString().length());
+            if (employee.getDaysString().length()>i
+                    && employee.getDaysString().charAt(i)=='1') {
+                checks[i].setChecked(true);//1 - это чекед
+            } else checks[i].setChecked(false);//иначе - анчекед
+        }
+    }
+
+    static void setDaysByCheckboxes(CheckBox[] checks, Employee employee) {
+        char[] str = new char[checks.length];
+        for (int i = 0; i < checks.length; i++) str[i]=checks[i].isChecked()?'1':'0';
+        String dayString = new String(str);
+        employee.setDaysString(dayString);
+        Log.e("TAG", "setDaysByCheckboxes: "+dayString);
+    }
+
+}
--- app/src/main/java/com/squorpikkor/magazassistant5/ui/main/adapter/InvoiceEmployeeAdapter.java
+++ app/src/main/java/com/squorpikkor/magazassistant5/ui/main/adapter/InvoiceEmployeeAdapter.java
@@ -1,7 +1,7 @@
 package com.squorpikkor.magazassistant5.ui.main.adapter;
 
+
 import android.annotation.SuppressLint;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +12,6 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.squorpikkor.magazassistant5.R;
-import com.squorpikkor.magazassistant5.ui.main.MainViewModel;
 import com.squorpikkor.magazassistant5.ui.main.entities.Employee;
 
 import java.util.ArrayList;
@@ -21,7 +20,6 @@ public class InvoiceEmployeeAdapter extends RecyclerView.Adapter<InvoiceEmployee
 
    private ArrayList<Employee> list;
    private int workingDaysCount = 5;
-   private MainViewModel mViewModel;
 
    @SuppressLint("NotifyDataSetChanged")
    public void setWorkingDaysCount(int workingDaysCount) {
@@ -29,15 +27,6 @@ public class InvoiceEmployeeAdapter extends RecyclerView.Adapter<InvoiceEmployee
       notifyDataSetChanged();
    }
 
-   //   public InvoiceEmployeeAdapter(MainViewModel mViewModel) {
-//      this.mViewModel = mViewModel;
-//      mViewModel.getWorkingDays().observe();
-//   }
-//
-//
-//   public InvoiceEmployeeAdapter(int workingDaysCount) {
-//      this.workingDaysCount = workingDaysCount;
-//   }
 
    @SuppressLint("NotifyDataSetChanged")
    public void setList(ArrayList<Employee> list) {
@@ -57,22 +46,7 @@ public class InvoiceEmployeeAdapter extends RecyclerView.Adapter<InvoiceEmployee
    public void onBindViewHolder(@NonNull AdapterViewHolder holder, int position) {
       Employee employee = list.get(position);
       holder.name.setText(employee.getName());
-      Log.e("TAG", "holder.checks.length: "+holder.checks.length);
-      int workingDayForEmployee = 0;
-      for (int i = 0; i < holder.checks.length; i++) {
-         //перебираем все 9 чекбоксов
-         holder.checks[i].setVisibility(workingDaysCount>i?View.VISIBLE:View.GONE);//оставляем количество чекбоксов равное количеству рабочих дней
-         if (employee.getDaysString().length()<i) holder.checks[i].setChecked(true);//если чекбоксов больше, чем сохраненных то все чекбоксы сверх сохраненных чекаем
-
-         Log.e("TAG", "*** employee.getDaysString().length(): "+employee.getDaysString().length());
-         if (employee.getDaysString().length()>i
-         && employee.getDaysString().charAt(i)=='1') {
-            holder.checks[i].setChecked(true);//1 - это чекед
-            workingDayForEmployee++;
-         } else holder.checks[i].setChecked(false);//иначе - анчекед
-      }
-      employee.setDays(workingDayForEmployee);//ХЗ
-
+      CheckBoxSwitcher.setCheckboxesByEmployee(holder.checks, employee, workingDaysCount);
    }
 
    @Override
@@ -88,7 +62,8 @@ public class InvoiceEmployeeAdapter extends RecyclerView.Adapter<InvoiceEmployee
       public AdapterViewHolder(@NonNull View itemView) {
          super(itemView);
          name = itemView.findViewById(R.id.text_name);
-         checks = new CheckBox[]{itemView.findViewById(R.id.check1),
+         checks = new CheckBox[]{
+                 itemView.findViewById(R.id.check1),
                  itemView.findViewById(R.id.check2),
                  itemView.findViewById(R.id.check3),
                  itemView.findViewById(R.id.check4),
@@ -97,6 +72,16 @@ public class InvoiceEmployeeAdapter extends RecyclerView.Adapter<InvoiceEmployee
                  itemView.findViewById(R.id.check7),
                  itemView.findViewById(R.id.check8),
                  itemView.findViewById(R.id.check9)};
+
+         for (CheckBox check : checks) {
+            check.setOnClickListener(view -> {
+               Employee employee = list.get(getAdapterPosition());
+               CheckBoxSwitcher.setDaysByCheckboxes(checks, employee);
+            });
+         }
       }
+
+
+
    }
 }
